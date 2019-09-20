3ad2f2d505475f2dcf14657e9d1505415991011a
sq1fya <rgpszczecin@gmail.com>
2019-09-20 22:47:03 +0200

    firebase + react

---
+++ src/config.js
@@ -0,0 +1,7 @@
+const config = {
+  apiKey: "{AIzaSyBrwpLJA7Jwup9qq88nxn_QXsuwI4dr95E}",
+  authDomain: "{clock-c8fe8.firebaseapp.com}",
+  databaseURL: "{https://clock-c8fe8.firebaseio.com/}",
+}
+
+export default config;
